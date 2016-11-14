db6e6bea214f9708c608915860a9885703189d0d
英孝 松村 <matsumura@tmnfsystems.com>
2016-11-14 02:25:03 -0600

    #4 バグ修正

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().print("Hello World!12345");
+        response.getWriter().print("Hello World!11111111111111111111111111111111111111111111111111111");
         /* testコメント */
     }
 
